068cc7cec29e3aadd344f07940740771b8b5792c
yashasviPearlOrg <yashasvi@pearlorganisation.com>
2023-02-23 16:48:22 +0530

    yashasvi

--- app/src/main/java/com/pearl/medicap/UI/Customer_Dashboard.kt
+++ app/src/main/java/com/pearl/medicap/UI/Customer_Dashboard.kt
@@ -38,40 +38,40 @@ import java.io.File
 import java.util.*
 
 
-class Customer_Dashboard :  BaseClass() {
+class Customer_Dashboard : BaseClass() {
     lateinit var binding: ActivityCustomerDashboardBinding
-    override var STORAGE_PERMISSION_CODE=1
+    override var STORAGE_PERMISSION_CODE = 1
     lateinit var multipleImageAdapter: MultipleImageAdapter
 
 
-    var imagelist=ArrayList<MultipleImage>()
-    var medicineList=ArrayList<String>()
-    var banner_lisst=ArrayList<Int>()
-    lateinit var input_medicine:EditText
-    lateinit var more_button:ImageView
-    lateinit var done_button:ImageView
-    lateinit var submit_button:Button
-    lateinit var drawer_button:ImageView
+    var imagelist = ArrayList<MultipleImage>()
+    var medicineList = ArrayList<String>()
+    var banner_lisst = ArrayList<Int>()
+    lateinit var input_medicine: EditText
+    lateinit var more_button: ImageView
+    lateinit var done_button: ImageView
+    lateinit var submit_button: Button
+    lateinit var drawer_button: ImageView
     lateinit var drawer: DrawerLayout
     lateinit var medicineAdapter: MedicineAdapter
     lateinit var prefManager: PrefManager
     lateinit var bannerAdapter: BannerAdapter
-    private var timer: CountDownTimer?=null
+    private var timer: CountDownTimer? = null
     lateinit var viewPagerAdapter: ImageSlideAdapter
     lateinit var indicator: CircleIndicator
     lateinit var comingSoonAdapter: ComingSoonAdapter
-    var comingsoonList=ArrayList<ComingSoon>()
+    var comingsoonList = ArrayList<ComingSoon>()
 
     var currentPage = 0
-    lateinit var timerr:Timer
+    lateinit var timerr: Timer
     val DELAY_MS: Long = 500
 
     val PERIOD_MS: Long = 3000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        prefManager= PrefManager(this)
-       prefManager.isCustomerlogin=false
+        prefManager = PrefManager(this)
+        prefManager.isCustomerlogin = false
         //prefManager.setCustomerlogin(false)
 
         setLayoutXml()
@@ -86,14 +86,13 @@ class Customer_Dashboard :  BaseClass() {
             initializeLabels()
             initializeInputs()
             printLogs("Customer_Dashboard", "onCreate", "exitConnected")
-        }
-        else{
-            Toast.makeText(this,"Please connnect with internet",Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Please connnect with internet", Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun changeStatusBarColor() {
-        window.statusBarColor= resources.getColor(R.color.App_color)
+        window.statusBarColor = resources.getColor(R.color.App_color)
     }
 
     override fun setLayoutXml() {
@@ -101,48 +100,49 @@ class Customer_Dashboard :  BaseClass() {
     }
 
     override fun initializeViews() {
-        input_medicine=findViewById(R.id.input_medicineET)
-       // more_button=findViewById(R.id.next_btnTV)
-        done_button=findViewById(R.id.done_btnTV)
-        submit_button=findViewById(R.id.submit_bt)
-        drawer_button=findViewById(R.id.sidebar)
-        drawer=findViewById(R.id.drawerr)
-        binding.medicinelist.layoutManager=LinearLayoutManager(this)
+        input_medicine = findViewById(R.id.input_medicineET)
+        // more_button=findViewById(R.id.next_btnTV)
+        done_button = findViewById(R.id.done_btnTV)
+        submit_button = findViewById(R.id.submit_bt)
+        drawer_button = findViewById(R.id.sidebar)
+        drawer = findViewById(R.id.drawerr)
+        binding.medicinelist.layoutManager = LinearLayoutManager(this)
     }
 
     override fun initializeClickListners() {
-      /*  drawer.addDrawerListener(action_bar_toggle)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)*/
+        /*  drawer.addDrawerListener(action_bar_toggle)
+          supportActionBar?.setDisplayHomeAsUpEnabled(true)*/
 
         binding.imageUploadLL.setOnClickListener {
             //Toast.makeText(this,"click",Toast.LENGTH_SHORT).show()
             requestPermission()
         }
-        done_button.setOnClickListener{
-            if (validateEmail(input_medicine)){
-            var data=input_medicine.text.toString()
-            medicineList.add(data)
-            medicineAdapter= MedicineAdapter(this,medicineList)
-            binding.medicinelist.adapter=medicineAdapter
-            hideSoftKeyboard(this,it )
-            input_medicine.clearFocus()
-            input_medicine.text.clear()
-         //   input_medicine.setHint("")
-        }
+        done_button.setOnClickListener {
+            if (validateEmail(input_medicine)) {
+                var data = input_medicine.text.toString()
+                medicineList.add(data)
+                medicineAdapter = MedicineAdapter(this, medicineList)
+                binding.medicinelist.adapter = medicineAdapter
+                hideSoftKeyboard(this, it)
+                input_medicine.clearFocus()
+                input_medicine.text.clear()
+                //   input_medicine.setHint("")
+            }
         }
         submit_button.setOnClickListener {
             val dialog = Dialog(this)
             dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
             dialog.setCancelable(false)
             dialog.setContentView(R.layout.waiting_dialog_layout)
-            var ok_button=dialog.findViewById<Button>(R.id.ok_btn)
+            var ok_button = dialog.findViewById<Button>(R.id.ok_btn)
             ok_button.setOnClickListener {
-                startActivity(Intent(this,CustomerBillDetailsActvity::class.java))
+                startActivity(Intent(this, CustomerBillDetailsActvity::class.java))
                 dialog.dismiss()
             }
             dialog.show()
 
-            Toast.makeText(this,"Your medicine Details successfully submitted",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Your medicine Details successfully submitted", Toast.LENGTH_SHORT)
+                .show()
         }
 
         drawer_button.setOnClickListener {
@@ -155,28 +155,29 @@ class Customer_Dashboard :  BaseClass() {
             alertDialog2.setTitle("Alert...")
             alertDialog2.setMessage("Are you sure you want to exit ?")
             alertDialog2.setPositiveButton("Yes") { dialog: DialogInterface?, which: Int ->
-                Toast.makeText(this,"Logout Successfully",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Logout Successfully", Toast.LENGTH_SHORT).show()
                 startActivity(Intent(this, LoginActivity::class.java))
             }
             alertDialog2.setNegativeButton(
                 "Cancel"
             ) { dialog: DialogInterface, which: Int -> dialog.cancel() }
             alertDialog2.show()
-               //  finish();
+            //  finish();
             true
 
         }
         binding.navigationView.menu.findItem(R.id.lab_test).setOnMenuItemClickListener {
-            Toast.makeText(this,"lab test",Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this,TestLabActivity::class.java))
+            Toast.makeText(this, "lab test", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, TestLabActivity::class.java))
             true
         }
         binding.navigationView.menu.findItem(R.id.customer_profile).setOnMenuItemClickListener {
-            startActivity(Intent(this,CustomerProfileActivity::class.java))
+            startActivity(Intent(this, CustomerProfileActivity::class.java))
             true
         }
 
     }
+
     fun hideSoftKeyboard(activity: Activity, view: View) {
         val imm: InputMethodManager =
             activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -184,16 +185,17 @@ class Customer_Dashboard :  BaseClass() {
     }
 
     override fun initializeInputs() {
-        prefManager=PrefManager(this)
-        binding.bannerList.layoutManager=LinearLayoutManager(this@Customer_Dashboard,LinearLayoutManager.HORIZONTAL,false)
-                banner_lisst.add(R.drawable.banner1)
-                banner_lisst.add(R.drawable.banner2)
-                banner_lisst.add(R.drawable.banner3)
-                banner_lisst.add(R.drawable.banner4)
-                banner_lisst.add(R.drawable.banner1)
+        prefManager = PrefManager(this)
+        binding.bannerList.layoutManager =
+            LinearLayoutManager(this@Customer_Dashboard, LinearLayoutManager.HORIZONTAL, false)
+        banner_lisst.add(R.drawable.banner1)
+        banner_lisst.add(R.drawable.banner2)
+        banner_lisst.add(R.drawable.banner3)
+        banner_lisst.add(R.drawable.banner4)
+        banner_lisst.add(R.drawable.banner1)
 
-        var bannerAdapter=BannerAdapter(this@Customer_Dashboard,banner_lisst)
-        binding.bannerList.adapter=bannerAdapter
+        var bannerAdapter = BannerAdapter(this@Customer_Dashboard, banner_lisst)
+        binding.bannerList.adapter = bannerAdapter
 
         viewPagerAdapter = ImageSlideAdapter(this, banner_lisst)
         binding.viewpager.adapter = viewPagerAdapter
@@ -210,23 +212,24 @@ class Customer_Dashboard :  BaseClass() {
             binding.viewpager.setCurrentItem(currentPage++, true)
         }
 
-      // This will create a new Thread
+        // This will create a new Thread
 
-        timerr=Timer()
-        timerr.schedule(object : TimerTask() {
-            // task to be scheduled
-            override fun run() {
-                handler.post(Update)
-            }
-        }, DELAY_MS, PERIOD_MS)
+//        timerr=Timer()
+//        timerr.schedule(object : TimerTask() {
+//            // task to be scheduled
+//            override fun run() {
+//                handler.post(Update)
+//            }
+//        }, DELAY_MS, PERIOD_MS)
 
 
-        binding.commingSoonList.layoutManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        comingsoonList.add(ComingSoon(R.drawable.ultrasound,"Ultrasound-Test"))
-        comingsoonList.add(ComingSoon(R.drawable.xray,"X-ray Test"))
-        comingsoonList.add(ComingSoon(R.drawable.ctscan,"Doctor Appointment"))
-        comingSoonAdapter=ComingSoonAdapter(this,comingsoonList)
-        binding.commingSoonList.adapter=comingSoonAdapter
+        binding.commingSoonList.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        comingsoonList.add(ComingSoon(R.drawable.ultrasound, "Ultrasound-Test"))
+        comingsoonList.add(ComingSoon(R.drawable.xray, "X-ray Test"))
+        comingsoonList.add(ComingSoon(R.drawable.ctscan, "Doctor Appointment"))
+        comingSoonAdapter = ComingSoonAdapter(this, comingsoonList)
+        binding.commingSoonList.adapter = comingSoonAdapter
 
     }
 
@@ -235,19 +238,34 @@ class Customer_Dashboard :  BaseClass() {
     }
 
     override fun requestPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             openFileExplorer()
             return;
         }
-        ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),STORAGE_PERMISSION_CODE)
+        ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        );
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            STORAGE_PERMISSION_CODE
+        )
     }
+
     override fun openFileExplorer() {
         val intent = Intent()
         intent.type = "image/*"
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), STORAGE_PERMISSION_CODE)
+        startActivityForResult(
+            Intent.createChooser(intent, "Select Picture"),
+            STORAGE_PERMISSION_CODE
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -261,14 +279,14 @@ class Customer_Dashboard :  BaseClass() {
                         imagelist.clear();
                         if (data!!.getClipData() != null) {
                             var count = data.getClipData()!!.getItemCount()
-                            for (i in 0..count-1 ) {
-                              var  imageUri: Uri = data.getClipData()!!.getItemAt(i).getUri()
+                            for (i in 0..count - 1) {
+                                var imageUri: Uri = data.getClipData()!!.getItemAt(i).getUri()
                                 imagelist.add(MultipleImage(imageUri))
 
                             }
-                            multipleImageAdapter= MultipleImageAdapter(this,imagelist)
-                            binding.imagelist.layoutManager=GridLayoutManager(this,4)
-                            binding.imagelist.adapter=multipleImageAdapter
+                            multipleImageAdapter = MultipleImageAdapter(this, imagelist)
+                            binding.imagelist.layoutManager = GridLayoutManager(this, 4)
+                            binding.imagelist.adapter = multipleImageAdapter
                         }
                     }
                 }
@@ -297,6 +315,7 @@ class Customer_Dashboard :  BaseClass() {
         assert(dir != null)
         return dir!!.delete()
     }
+
     fun clearApplicationData() {
         val cache = cacheDir
         val appDir = File(cache.parent)
